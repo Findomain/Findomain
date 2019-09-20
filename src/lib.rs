Findomain/Findomain

commit 9af08af4416002b23e0fab1099187571d2b16c66

Introduce error handling.

src/lib.rs
@@ -18,6 +18,8 @@ use std::{
 use trust_dns_resolver::{config::ResolverConfig, config::ResolverOpts, Resolver};
 
 mod auth;
+pub mod errors;
+use crate::errors::*;
 
 #[derive(Deserialize, Eq, PartialEq, Hash)]
 struct SubdomainsCertSpotter {
@@ -87,7 +89,7 @@ lazy_static! {
         .unwrap();
 }
 
-pub fn get_subdomains(target: &str, with_ip: &str, with_output: &str, file_name: &str) {
+pub fn get_subdomains(target: &str, with_ip: &str, with_output: &str, file_name: &str) -> Result<()> {
     let target = target
         .replace("www.", "")
         .replace("https://", "")
@@ -211,7 +213,7 @@ pub fn get_subdomains(target: &str, with_ip: &str, with_output: &str, file_name:
         &with_ip,
         &with_output,
         &file_name,
-    );
+    )?;
     if with_ip == "y" && with_output == "y" {
         println!(
             ">> 📁 Filename for the target {} was saved in: ./{} 😀",
@@ -223,6 +225,7 @@ pub fn get_subdomains(target: &str, with_ip: &str, with_output: &str, file_name:
             &target, &file_name
         )
     }
+    Ok(())
 }
 
 fn manage_subdomains_data(
@@ -231,7 +234,7 @@ fn manage_subdomains_data(
     with_ip: &str,
     with_output: &str,
     file_name: &str,
-) {
+) -> Result<()> {
     let base_target = [".", &target].concat();
     if subdomains.is_empty() {
         println!(
@@ -239,14 +242,14 @@ fn manage_subdomains_data(
             &target
         );
     } else {
-        check_output_file_exists(&file_name);
+        check_output_file_exists(&file_name)?;
         subdomains.retain(|sub| {
             !sub.contains("*") && !sub.starts_with(".") && sub.contains(&base_target)
         });
         if with_ip == "y" && with_output == "y" {
             for subdomain in &subdomains {
                 let ipadress = get_ip(&subdomain);
-                write_to_file(&subdomain, &ipadress, &file_name, &with_ip);
+                write_to_file(&subdomain, &ipadress, &file_name, &with_ip)?;
                 println!("{},{}", &subdomain, &ipadress);
             }
         } else if with_ip == "y" && with_output != "y" {
@@ -257,7 +260,7 @@ fn manage_subdomains_data(
         } else if with_ip != "y" && with_output == "y" {
             let ipadress = "";
             for subdomain in &subdomains {
-                write_to_file(&subdomain, &ipadress, &file_name, &with_ip);
+                write_to_file(&subdomain, &ipadress, &file_name, &with_ip)?;
                 println!("{}", &subdomain);
             }
         } else {
@@ -272,6 +275,8 @@ fn manage_subdomains_data(
         );
         println!("\nGood luck Hax0r 💀!\n");
     }
+
+    Ok(())
 }
 
 fn get_certspotter_subdomains(url_api_certspotter: &str) -> Option<HashSet<String>> {
@@ -541,64 +546,37 @@ fn check_json_errors(error: reqwest::Error, api: &str) {
     println!("An error ❌ occurred while parsing the JSON obtained from the {} API. Error description: {}.", &api, error.description())
 }
 
-pub fn read_from_file(file: &str, with_ip: &str, with_output: &str) {
+pub fn read_from_file(file: &str, with_ip: &str, with_output: &str) -> Result<()> {
     match File::open(&file) {
         Ok(f) => {
             let f = BufReader::new(f);
             for domain in f.lines() {
                 let domain = domain.unwrap().to_string();
                 let file_name = [&domain, ".txt"].concat();
-                get_subdomains(&domain, &with_ip, &with_output, &file_name)
+                get_subdomains(&domain, &with_ip, &with_output, &file_name)?;
             }
         }
         Err(e) => {
             println!("Can't open file 📁 {}. Error: {}", &file, e.description());
         }
     }
+    Ok(())
 }
 
-fn write_to_file(data: &str, subdomain_ip: &str, file_name: &str, with_ip: &str) {
-    if with_ip == "y" {
-        let data = &[data, ",", subdomain_ip, "\n"].concat();
-        if Path::new(&file_name).exists() {
-            let mut output_file = OpenOptions::new()
-                .append(true)
-                .open(&file_name)
-                .expect("Can't open file.");
-            output_file
-                .write_all(&data.as_bytes())
-                .expect("Failed writing to file.");
-        } else {
-            File::create(&file_name).expect("Failed to create file.");
-            let mut output_file = OpenOptions::new()
-                .append(true)
-                .open(&file_name)
-                .expect("Can't open file.");
-            output_file
-                .write_all(&data.as_bytes())
-                .expect("Failed writing to file.");
-        }
+fn write_to_file(data: &str, subdomain_ip: &str, file_name: &str, with_ip: &str) -> Result<()> {
+    let data = if with_ip == "y" {
+        [data, ",", subdomain_ip, "\n"].concat()
     } else {
-        let data = &[data, "\n"].concat();
-        if Path::new(&file_name).exists() {
-            let mut output_file = OpenOptions::new()
-                .append(true)
-                .open(&file_name)
-                .expect("Can't open file.");
-            output_file
-                .write_all(&data.as_bytes())
-                .expect("Failed writing to file.");
-        } else {
-            File::create(&file_name).expect("Failed to create file.");
-            let mut output_file = OpenOptions::new()
-                .append(true)
-                .open(&file_name)
-                .expect("Can't open file.");
-            output_file
-                .write_all(&data.as_bytes())
-                .expect("Failed writing to file.");
-        }
-    }
+        [data, "\n"].concat()
+    };
+    let mut output_file = OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open(&file_name)
+        .context("Can't open file")?;
+    output_file
+        .write_all(&data.as_bytes())?;
+    Ok(())
 }
 
 fn get_ip(domain: &str) -> String {
@@ -628,21 +606,12 @@ fn get_resolver() -> Resolver {
     }
 }
 
-pub fn check_output_file_exists(file_name: &str) {
+pub fn check_output_file_exists(file_name: &str) -> Result<()> {
     if Path::new(&file_name).exists() && Path::new(&file_name).is_file() {
-        match fs::rename(
+        fs::rename(
             &file_name,
             &file_name.replace(&file_name.split(".").last().unwrap(), "old.txt"),
-        ) {
-            Ok(_) => (),
-            Err(e) => {
-                println!(
-                    "An error occurred while backing up the file {}. Error: {}.",
-                    &file_name,
-                    e.description(),
-                );
-                std::process::exit(1)
-            }
-        }
+        ).with_context(|_| format!("An error occurred while backing up the file {:?}", &file_name))?;
     }
+    Ok(())
 }

src/main.rs
@@ -3,8 +3,9 @@ extern crate clap;
 use clap::App;
 
 use findomain::{get_subdomains, read_from_file};
+use findomain::errors::*;
 
-fn main() {
+fn run() -> Result<()> {
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
     if matches.is_present("target") && matches.is_present("output") {
@@ -49,5 +50,17 @@ fn main() {
             let with_ip = "";
             read_from_file(&file, &with_ip, &with_output)
         }
+    } else {
+        Ok(())
+    }
+}
+
+fn main() {
+    if let Err(err) = run() {
+        eprintln!("Error: {}", err);
+        for cause in err.iter_chain().skip(1) {
+            eprintln!("Because: {}", cause);
+        }
+        std::process::exit(1);
     }
 }

Cargo.toml
@@ -18,6 +18,7 @@ clap = { version = "2.33.0", features = ["yaml"] }
 lazy_static = "1.4.0"
 rand = "0.7.2"
 postgres = "0.15.2"
+failure = "0.1"
 
 [target.arm-unknown-linux-gnueabihf.dependencies]
 openssl = { version = "0.10.24", features = ["vendored"] }

src/errors.rs
@@ -1,0 +1,2 @@
+pub use failure::{Error, ResultExt};
+pub type Result<T> = ::std::result::Result<T, Error>;

commit ee90b3307d531658faac1df9aff76829bf51035b

Improve error handling introduced in 9af08af.

src/lib.rs
@@ -89,7 +89,12 @@ lazy_static! {
         .unwrap();
 }
 
-pub fn get_subdomains(target: &str, with_ip: &str, with_output: &str, file_name: &str) -> Result<()> {
+pub fn get_subdomains(
+    target: &str,
+    with_ip: &str,
+    with_output: &str,
+    file_name: &str,
+) -> Result<()> {
     let target = target
         .replace("www.", "")
         .replace("https://", "")
@@ -269,7 +274,7 @@ fn manage_subdomains_data(
             }
         }
         println!(
-            "\nA total of {} subdomains were found for ==>  {} 👽\n",
+            "\nA total of {} subdomains were found for ==>  {} 👽",
             &subdomains.len(),
             &target
         );
@@ -341,7 +346,7 @@ fn get_crtsh_db_subdomains(crtsh_db_query: &str, url_api_crtsh: &str) -> Option<
             ),
             Err(e) => {
                 println!(
-                    "A error ❌ has occurred while querying the Crtsh database. Error: {}. Trying the API method...",
+                    "❌ A error has occurred while querying the Crtsh database. Error: {}. Trying the API method...",
                     e.description()
                 );
                 get_crtsh_subdomains(&url_api_crtsh)
@@ -349,7 +354,7 @@ fn get_crtsh_db_subdomains(crtsh_db_query: &str, url_api_crtsh: &str) -> Option<
         },
         Err(e) => {
             println!(
-                "A error ❌ has occurred while connecting to the Crtsh database. Error: {}. Trying the API method...",
+                "❌ A error has occurred while connecting to the Crtsh database. Error: {}. Trying the API method...",
                 e.description()
             );
             get_crtsh_subdomains(&url_api_crtsh)
@@ -513,29 +518,29 @@ fn get_virustotal_apikey_subdomains(url_virustotal_apikey: &str) -> Option<HashS
 fn check_request_errors(error: reqwest::Error, api: &str) {
     if error.is_timeout() {
         println!(
-            "A timeout ⏳ error has occurred while processing the request in the {} API. Error description: {}",
+            "⏳ A timeout error has occurred while processing the request in the {} API. Error description: {}",
             &api, &error.description())
     } else if error.is_redirect() {
         println!(
-            "A redirect ↪️  was found while processing the {} API. Error description: {}",
+            "❌ A redirect was found while processing the {} API. Error description: {}",
             &api,
             &error.description()
         )
     } else if error.is_client_error() {
         println!(
-            "A client error 🧑❌ has occurred sending the request to the {} API. Error description: {}",
+            "❌ A client error has occurred sending the request to the {} API. Error description: {}",
             &api,
             &error.description()
         )
     } else if error.is_server_error() {
         println!(
-            "A server error ❌ has occurred sending the request to the {} API. Error description: {}",
+            "❌ A server error has occurred sending the request to the {} API. Error description: {}",
             &api,
             &error.description()
         )
     } else {
         println!(
-            "An error ❌ has occurred while procesing the request in the {} API. Error description: {}",
+            "❌ An error has occurred while procesing the request in the {} API. Error description: {}",
             &api,
             &error.description()
         )
@@ -543,7 +548,7 @@ fn check_request_errors(error: reqwest::Error, api: &str) {
 }
 
 fn check_json_errors(error: reqwest::Error, api: &str) {
-    println!("An error ❌ occurred while parsing the JSON obtained from the {} API. Error description: {}.", &api, error.description())
+    println!("❌ An error occurred while parsing the JSON obtained from the {} API. Error description: {}.", &api, error.description())
 }
 
 pub fn read_from_file(file: &str, with_ip: &str, with_output: &str) -> Result<()> {
@@ -573,9 +578,8 @@ fn write_to_file(data: &str, subdomain_ip: &str, file_name: &str, with_ip: &str)
         .append(true)
         .create(true)
         .open(&file_name)
-        .context("Can't open file")?;
-    output_file
-        .write_all(&data.as_bytes())?;
+        .with_context(|_| format!("Can't open file: {}", &file_name))?;
+    output_file.write_all(&data.as_bytes())?;
     Ok(())
 }
 
@@ -608,10 +612,13 @@ fn get_resolver() -> Resolver {
 
 pub fn check_output_file_exists(file_name: &str) -> Result<()> {
     if Path::new(&file_name).exists() && Path::new(&file_name).is_file() {
-        fs::rename(
-            &file_name,
-            &file_name.replace(&file_name.split(".").last().unwrap(), "old.txt"),
-        ).with_context(|_| format!("An error occurred while backing up the file {:?}", &file_name))?;
+        let backup_file_name = file_name.replace(&file_name.split(".").last().unwrap(), "old.txt");
+        fs::rename(&file_name, &backup_file_name).with_context(|_| {
+            format!(
+                "The file {} already exists but Findomain can't backup the file to {}. Please run the tool with a more privileged user or try in a different directory.",
+                &file_name, &backup_file_name,
+            )
+        })?;
     }
     Ok(())
 }

src/main.rs
@@ -2,8 +2,8 @@
 extern crate clap;
 use clap::App;
 
-use findomain::{get_subdomains, read_from_file};
 use findomain::errors::*;
+use findomain::{get_subdomains, read_from_file};
 
 fn run() -> Result<()> {
     let yaml = load_yaml!("cli.yml");
@@ -57,9 +57,9 @@ fn run() -> Result<()> {
 
 fn main() {
     if let Err(err) = run() {
-        eprintln!("Error: {}", err);
+        eprintln!("\nError: {}", err);
         for cause in err.iter_chain().skip(1) {
-            eprintln!("Because: {}", cause);
+            eprintln!("Error description: {}", cause);
         }
         std::process::exit(1);
     }