1e6f88698e7f5fad49df2e58b2497f80fe554004
Eduard Tolosa <edu4rdshl@protonmail.com>
2019-11-02 14:40:25 -0500

    Read environment variables only when needed.

--- src/args.rs
+++ src/args.rs
@@ -1,4 +1,4 @@
-use crate::{get_vars, misc::sanitize_target_string};
+use crate::misc::sanitize_target_string;
 use clap::{load_yaml, value_t, App};
 use std::collections::HashSet;
 
@@ -54,11 +54,11 @@ pub fn get_args() -> Args {
             value_t!(matches, "postgres-port", usize).unwrap_or_else(|_| 5432),
             value_t!(matches, "postgres-database", String).unwrap_or_else(|_| String::new()),
         ),
-        discord_webhook: get_vars::get_webhook("discord"),
-        slack_webhook: get_vars::get_webhook("slack"),
-        telegram_bot_token: get_vars::get_auth_token("telegram"),
+        discord_webhook: String::new(),
+        slack_webhook: String::new(),
+        telegram_bot_token: String::new(),
         telegram_webhook: String::new(),
-        telegram_chat_id: get_vars::get_chat_id("telegram"),
+        telegram_chat_id: String::new(),
 
         only_resolved: matches.is_present("resolved"),
         with_ip: matches.is_present("ip"),
