Findomain/Findomain

commit 0c8a87d9ca4e3f90fd188c44807c9ef6a98446a9

Do not force insert of root target.
Forcing the insert lead to false positives when you search for a invalid subdomain because there are not checks but just force the insert so, we never had a target with no subdomains found.

src/lib.rs
@@ -157,7 +157,6 @@ fn search_subdomains(args: &mut args::Args) -> HashSet<String> {
     ].into_iter().map(|j| j.join().unwrap()).collect::<Vec<_>>().into_iter().flatten().flatten().collect();
 
     all_subdomains.retain(|sub| misc::validate_subdomain(&base_target, &sub));
-    all_subdomains.insert(args.target.clone());
     all_subdomains
 }
 

src/misc.rs
@@ -215,7 +215,7 @@ pub fn return_facebook_token() -> String {
 pub fn validate_subdomain(base_target: &str, subdomain: &str) -> bool {
     !subdomain.is_empty()
         && !subdomain.starts_with('.')
-        && subdomain.ends_with(base_target)
+        && (subdomain.ends_with(base_target) || subdomain == args.target)
         && !subdomain.contains(&SPECIAL_CHARS[..])
         && subdomain.chars().all(|c| c.is_ascii())
 }

commit 7d74c11ad81a6cf7a61d33f3691056efd6f9b4ca

Fixes bug introduced in https://github.com/Edu4rdSHL/findomain/commit/0c8a87d9ca4e3f90fd188c44807c9ef6a98446a9

src/lib.rs
@@ -156,7 +156,7 @@ fn search_subdomains(args: &mut args::Args) -> HashSet<String> {
         thread::spawn(move || sources::get_threatminer_subdomains(&url_api_threatminer, quiet_flag)),
     ].into_iter().map(|j| j.join().unwrap()).collect::<Vec<_>>().into_iter().flatten().flatten().collect();
 
-    all_subdomains.retain(|sub| misc::validate_subdomain(&base_target, &sub));
+    all_subdomains.retain(|sub| misc::validate_subdomain(&base_target, &sub, args));
     all_subdomains
 }
 

src/misc.rs
@@ -212,7 +212,7 @@ pub fn return_facebook_token() -> String {
     findomain_fb_tokens[rand::thread_rng().gen_range(0, findomain_fb_tokens.len())].to_string()
 }
 
-pub fn validate_subdomain(base_target: &str, subdomain: &str) -> bool {
+pub fn validate_subdomain(base_target: &str, subdomain: &str, args: &mut args::Args) -> bool {
     !subdomain.is_empty()
         && !subdomain.starts_with('.')
         && (subdomain.ends_with(base_target) || subdomain == args.target)