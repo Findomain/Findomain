683a0bb25e23b7323e3888cef9e576f3652defbe
Eduard Tolosa <edu4rdshl@protonmail.com>
2020-07-21 12:27:38 -0500

    Bump version.

--- builder.sh
+++ builder.sh
@@ -1,5 +1,6 @@
 # Findomain releaser
 WIN_TARGET="x86_64-pc-windows-gnu"
+RPI_TARGET="armv7-unknown-linux-gnueabihf"
 AARCH_TARGET="aarch64-unknown-linux-gnu"
 OSX_TARGET="x86_64-apple-darwin"
 MANPAGE_DIR="./findomain.1"
@@ -15,16 +16,25 @@ fi
 
 # Windows build
 echo "Building Windows artifact."
-if cargo build -q --release --target="$WIN_TARGET"; then
+if cross build -q --release --target="$WIN_TARGET"; then
   echo "Windows artifact build: SUCCESS"
   cp "target/$WIN_TARGET/release/findomain.exe" "target/$WIN_TARGET/release/findomain-windows.exe"
 else
   echo "Windows artifact build: FAILED"
 fi
 
+# RPi 2-3 build
+echo "Building RPi artifact."
+if cross build -q --release --target="$RPI_TARGET"; then
+  echo "Windows artifact build: SUCCESS"
+  cp "target/$RPI_TARGET/release/findomain.exe" "target/$RPI_TARGET/release/findomain-rpi"
+else
+  echo "Windows artifact build: FAILED"
+fi
+
 # Aarch64 build
 echo "Building Aarch64 artifact."
-if cargo build -q --release --target="$AARCH_TARGET"; then
+if cross build -q --release --target="$AARCH_TARGET"; then
   echo "Aarch64 artifact build: SUCCESS"
   cp "target/$AARCH_TARGET/release/findomain" "target/$AARCH_TARGET/release/findomain-aarch64"
 else
@@ -33,7 +43,7 @@ fi
 
 # Mac OS build
 echo "Building OSX artifact."
-if CC=o64-clang CXX=o64-clang++ LIBZ_SYS_STATIC=1 cargo build -q --release --target="$OSX_TARGET"; then
+if cross build -q --release --target="$OSX_TARGET"; then
   echo "OSX artifact build: SUCCESS"
   cp "target/$OSX_TARGET/release/findomain" "target/$OSX_TARGET/release/findomain-osx"
 else
