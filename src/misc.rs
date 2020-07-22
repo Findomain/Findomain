b91752022da5daecb1a46c21f7d42e8117479d5d
Eduard Tolosa <edu4rdshl@protonmail.com>
2020-07-22 11:46:34 -0500

    Add subdomains starting with `*.` to scope. This commit allow subdomains starting with `*.` to be in scope simply replacing that characters from the subdomain. Fixes #107

--- src/misc.rs
+++ src/misc.rs
@@ -213,6 +213,12 @@ pub fn return_facebook_token() -> String {
 }
 
 pub fn validate_subdomain(base_target: &str, subdomain: &str, args: &mut args::Args) -> bool {
+    let subdomain = if subdomain.starts_with("*.") {
+        subdomain.replace("*.", "")
+    } else {
+        subdomain.to_string()
+    };
+
     !subdomain.is_empty()
         && !subdomain.starts_with('.')
         && (subdomain.ends_with(base_target) || subdomain == args.target)
