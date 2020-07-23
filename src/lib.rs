Findomain/Findomain

commit b91752022da5daecb1a46c21f7d42e8117479d5d

Add subdomains starting with `*.` to scope. This commit allow subdomains starting with `*.` to be in scope simply replacing that characters from the subdomain. Fixes #107

src/misc.rs
@@ -213,6 +213,12 @@ pub fn return_facebook_token() -> String {
 }
 
 pub fn validate_subdomain(base_target: &str, subdomain: &str, args: &mut args::Args) -> bool {
+    let subdomain = if subdomain.starts_with("*.") {
+        subdomain.replace("*.", "")
+    } else {
+        subdomain.to_string()
+    };
+
     !subdomain.is_empty()
         && !subdomain.starts_with('.')
         && (subdomain.ends_with(base_target) || subdomain == args.target)

commit f592c68a60365cc6c2031430205e3007aefdfd28

Fix problem with unwanted data being send to DB and output. The problem was introduced in https://github.com/Edu4rdSHL/findomain/commit/b91752022da5daecb1a46c21f7d42e8117479d5d

src/lib.rs
@@ -225,7 +225,7 @@ fn search_subdomains(args: &mut args::Args) -> HashSet<String> {
                 sources::get_c99_subdomains(&url_api_c99, quiet_flag)
             })
         }
-    ].into_iter().map(|j| j.join().unwrap()).collect::<Vec<_>>().into_iter().flatten().flatten().map(|sub| sub.to_lowercase()).collect();
+    ].into_iter().map(|j| j.join().unwrap()).collect::<Vec<_>>().into_iter().flatten().flatten().map(|sub| misc::sanitize_subdomains(&sub)).collect();
 
     all_subdomains.retain(|sub| misc::validate_subdomain(&base_target, &sub, args));
     all_subdomains

src/misc.rs
@@ -213,17 +213,19 @@ pub fn return_facebook_token() -> String {
 }
 
 pub fn validate_subdomain(base_target: &str, subdomain: &str, args: &mut args::Args) -> bool {
-    let subdomain = if subdomain.starts_with("*.") {
-        subdomain.replace("*.", "")
-    } else {
-        subdomain.to_string()
-    };
-
     !subdomain.is_empty()
         && !subdomain.starts_with('.')
         && (subdomain.ends_with(base_target) || subdomain == args.target)
         && !subdomain.contains(&SPECIAL_CHARS[..])
         && subdomain.chars().all(|c| c.is_ascii())
+}
+
+pub fn sanitize_subdomains(subdomain: &str) -> String {
+    if subdomain.starts_with("*.") {
+        subdomain.replace("*.", "").to_lowercase()
+    } else {
+        subdomain.to_string().to_lowercase()
+    }
 }
 
 pub fn check_http_response_code(