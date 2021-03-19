354f10bf4728b2b0c2cab44382441b0764b38a44
Edu4rdSHL <edu4rdshl@protonmail.com>
2021-03-18 22:53:45 -0500

    Optimize binari size. Add 383 linux binary.

--- builder.sh
+++ builder.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # Findomain releaser
 LINUX_TARGET="x86_64-unknown-linux-musl"
+LINUX_X86_TARGET="i686-unknown-linux-musl"
 WIN_TARGET="x86_64-pc-windows-gnu"
 RPI_TARGET="armv7-unknown-linux-gnueabihf"
 AARCH_TARGET="aarch64-unknown-linux-gnu"
@@ -11,9 +12,20 @@ MANPAGE_DIR="./findomain.1"
 echo "Building Linux artifact."
 if cargo build -q --release --target="$LINUX_TARGET"; then
   echo "Linux artifact build: SUCCESS"
-  cp "target/$LINUX_TARGET/release/findomain" "target/$LINUX_TARGET/release/findomain-linux"
+  cp "target/$LINUX_TARGET/release/findomain" "target/$LINUX_TARGET/release/findomain-linux-amd64"
+  strip "target/$LINUX_TARGET/release/findomain-linux-amd64"
 else
   echo "Linux artifact build: FAILED"
+fi
+
+# Linux x86 build
+echo "Building Linux artifact."
+if cross build -q --release --target="$LINUX_X86_TARGET"; then
+  echo "Linux x86 artifact build: SUCCESS"
+  cp "target/$LINUX_X86_TARGET/release/findomain" "target/$LINUX_X86_TARGET/release/findomain-linux-i386"
+  strip "target/$LINUX_X86_TARGET/release/findomain-linux-i386"
+else
+  echo "Linux x86 artifact build: FAILED"
 fi
 
 # Windows build
@@ -21,6 +33,7 @@ echo "Building Windows artifact."
 if cross build -q --release --target="$WIN_TARGET"; then
   echo "Windows artifact build: SUCCESS"
   cp "target/$WIN_TARGET/release/findomain.exe" "target/$WIN_TARGET/release/findomain-windows.exe"
+  strip "target/$WIN_TARGET/release/findomain-windows.exe"
 else
   echo "Windows artifact build: FAILED"
 fi
@@ -30,6 +43,7 @@ echo "Building RPi artifact."
 if cross build -q --release --target="$RPI_TARGET"; then
   echo "Windows artifact build: SUCCESS"
   cp "target/$RPI_TARGET/release/findomain" "target/$RPI_TARGET/release/findomain-rpi"
+  strip "target/$RPI_TARGET/release/findomain-rpi"
 else
   echo "RPi artifact build: FAILED"
 fi
@@ -39,6 +53,7 @@ echo "Building Aarch64 artifact."
 if cross build -q --release --target="$AARCH_TARGET"; then
   echo "Aarch64 artifact build: SUCCESS"
   cp "target/$AARCH_TARGET/release/findomain" "target/$AARCH_TARGET/release/findomain-aarch64"
+  strip "target/$AARCH_TARGET/release/findomain-aarch64"
 else
   echo "Aarch64 artifact build: FAILED"
 fi
@@ -48,6 +63,7 @@ echo "Building OSX artifact."
 if CC=o64-clang CXX=o64-clang++ LIBZ_SYS_STATIC=1 cargo build -q --release --target="$OSX_TARGET"; then
   echo "OSX artifact build: SUCCESS"
   cp "target/$OSX_TARGET/release/findomain" "target/$OSX_TARGET/release/findomain-osx"
+  strip "target/$OSX_TARGET/release/findomain-osx"
 else
   echo "OSX artifact build: FAILED"
 fi
@@ -67,14 +83,6 @@ if command -v git >/dev/null; then
   git add .
   git commit -m "Bump version."
   git push
-fi
-
-echo "Uploading crate to crates.io..."
-if cargo publish --no-verify >/dev/null; then
-  echo "Crate uploaded."
-else
-  echo "An error has occurred while uploading the crate to crates.io."
-  exit
 fi
 
 echo "All builds have passed!"
