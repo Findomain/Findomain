c6eb2f94f4c4aeaf4b4b4b138a9adbdc3cf9ef24
Edu4rdSHL <edu4rdshl@protonmail.com>
2021-12-12 20:56:54 -0500

    Simplify webhooks checking logic.
    Signed-off-by: Edu4rdSHL <edu4rdshl@protonmail.com>

--- src/errors.rs
+++ src/errors.rs
@@ -17,23 +17,18 @@ pub fn check_json_errors(error: reqwest::Error, api: &str) {
 }
 
 pub fn check_monitoring_parameters(args: &mut Args) -> Result<()> {
-    if args.discord_webhook.is_empty()
-        && args.slack_webhook.is_empty()
-        && args.telegram_bot_token.is_empty()
-        && args.telegram_chat_id.is_empty()
-    {
-        eprintln!("You need to configure at least one webhook variable in your system. For Discord set the findomain_discord_webhook system variable, for Slack set the findomain_slack_webhook variable, for Telegram set the findomain_telegrambot_token and findomain_telegrambot_chat_id variables. See https://git.io/JeZQW for more information, exiting.");
-        std::process::exit(1)
-    } else if (args.telegram_bot_token.is_empty() && !args.telegram_chat_id.is_empty())
-        || (!args.telegram_bot_token.is_empty() && args.telegram_chat_id.is_empty())
-    {
-        eprintln!("You need to set the findomain_telegrambot_chat_id an findomain_telegrambot_token variables if you want to use Telegram webhooks. See https://git.io/JeZQW for more information, exiting.");
-        std::process::exit(1)
-    } else if !args.telegram_bot_token.is_empty() && !args.telegram_chat_id.is_empty() {
+    if !args.telegram_bot_token.is_empty() && !args.telegram_chat_id.is_empty() {
         args.telegram_webhook = format!(
             "https://api.telegram.org/bot{}/sendMessage",
             args.telegram_bot_token
         )
+    }
+    if args.discord_webhook.is_empty()
+        && args.slack_webhook.is_empty()
+        && args.telegram_webhook.is_empty()
+    {
+        eprintln!("You must provide a webhook to use the monitoring API.");
+        std::process::exit(1)
     }
     Ok(())
 }
